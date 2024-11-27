8032f3e0ba7aa3b1c55c9005f8937ffe681e1b77
Thomas Juhnke <tommy@van-tomas.de>
2024-11-27 22:47:30 +0100

    Move JavaScript to external file

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Masonry layout with CSS grid.
+// Based on:
+// - https://css-tricks.com/piecing-together-approaches-for-a-css-masonry-layout/#aa-grid-javascript-manipulated-row-spans
+// - https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
+function resizeGridItem(item) {
+  grid = item.parentNode;
+  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+  item.style.gridRowEnd = "span " + rowSpan;
+}
+
+function resizeAllGridItems() {
+  allItems = document.querySelectorAll(".recent-category--grid .item");
+  for (x = 0; x < allItems.length; x++) {
+    resizeGridItem(allItems[x]);
+  }
+}
+
+function resizeInstance(instance) {
+  item = instance.elements[0];
+  resizeGridItem(item);
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
